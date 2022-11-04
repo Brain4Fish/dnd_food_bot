ef70dae4e1781c1bdf857643108da57c1c25c80d
Mikhail Kozlov <empty@brain4.fish>
2022-11-04 13:34:01 +0300

    add fetch and exec functions for db

--- bot.py
+++ bot.py
@@ -7,13 +7,7 @@ import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
 
 ##
 # Database functions
@@ -47,22 +41,61 @@ class DbConnect(object):
             self.dbconn.close()
 
 
-def create_food_table():
+def db_exec(query):
     '''
-    Create db table
+    Executing database query
     '''
-    query = '''CREATE TABLE IF NOT EXISTS food (
-                    date TEXT NOT NULL,
-                    who TEXT NOT NULL,
-                    type TEXT NOT NULL);'''
     with DbConnect() as sql_connection:
         db_connection, cursor = sql_connection
         cursor.execute(query)
         db_connection.commit()
 
 
+def db_fetch(query):
+    '''
+    Executing database fetch query
+    '''
+    with DbConnect() as sql_connection:
+        _, cursor = sql_connection
+        cursor.execute(query)
+        selected_data = cursor.fetchall()
+    return selected_data
+
+
+def create_food_table():
+    '''
+    Create food table in database
+    '''
+    query = '''CREATE TABLE IF NOT EXISTS food (
+                    date TEXT NOT NULL,
+                    who TEXT NOT NULL,
+                    type TEXT NOT NULL);'''
+    db_exec(query)
+
+
+def get_food_orders():
+    '''
+    Fetch food orders from database
+    '''
+    query = f'''SELECT * FROM (SELECT rowid, date, who, type from food ORDER BY rowid DESC LIMIT 10) ORDER BY rowid ASC;'''
+    return db_fetch(query)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(commands=['orders'])
+def food_orders(message):
+    bot.reply_to(message, str(get_food_orders()))
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
 def main():
     create_food_table()
+    print(get_food_orders())
     bot.set_my_commands([
         telebot.types.BotCommand("/start", "main menu"),
         telebot.types.BotCommand("/help", "print usage")
